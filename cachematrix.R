c513ca9546720693da5d4fcd69d0d5011adf0435
ppetto <ppetto@ppetto.com>
2014-07-25 16:12:12 -0400

    completed assignment
    rewrote and tested exemplary functions (from vector ~ mean) to
    invertible matrix ~ inverse…two tests, 2x2 and 3x3; then added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## These functions are analgous to the exemplary makeVector and cachemean functions
-## given as examples, rejiggered to work on square matrices, computing an inverse instead of a mean
+## These functions work similarly to the example makeVector and cachemean functions, but have been
+## rewritten to work on square invertible matrices, computing an inverse instead of a mean.
 
-## Write a short comment describing this function
+## They DO NOT check the input to make sure it is a square invertible matrix.
+
+
+## This is a function-containing list that includes four functions to:
+## set - set the value of the matrix
+## get - get the value of the matrix
+## setinverse - set the value of the inverse
+## getinverse - get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      mat <- NULL
+      set <- function(y) {
+            x <<- y
+            mat <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) mat <<- solve
+      getinverse <- function() mat
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function calculates the inverse of the special "matrix" created with the above function,
+## if it has NOT already been calculated -- if it has, it retrieves the inverse from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      mat <- x$getinverse()
+      if(!is.null(mat)) {
+            message("getting cached data")
+            return(mat)
+      }
+      data <- x$get()
+      mat <- solve(data, ...)
+      x$setinverse(mat)
+      mat
 }
+c
